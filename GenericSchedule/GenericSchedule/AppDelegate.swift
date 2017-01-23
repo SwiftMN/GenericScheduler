10de4b8380324a4e707c2bb7a455d6d0ce584e34
Adam Ahrens <adam.aahrens@gmail.com>
2017-01-23 11:17:25 -0600

    Initial Project

---
+++ GenericSchedule/GenericSchedule/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  GenericSchedule
+//
+//  Created by Adam Ahrens on 1/23/17.
+//  Copyright © 2017 Adam Ahrens. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+    return true
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+
+  }
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+
+  }
+
+  func applicationDidBecomeActive(_ application: UIApplication) {
+
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
+
+  }
+
+}
+
